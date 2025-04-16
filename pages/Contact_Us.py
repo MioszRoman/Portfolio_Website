aa2211a84fa162b4cb63ca0b9b341b10793923a4
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-16 18:38:24 +0200

    Refactor a structure

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -7,5 +7,11 @@ st.title("Contact Us")
 
 with st.form(key="email_forms"):
     user = st.text_input("Give us your email: ")
-    message = st.text_area("Write your message:")
+    raw_message = st.text_area("Write your message:")
+    message = f"""\
+Subject: New message from {user}\n
+From: {user}
+{raw_message}
+"""
+
     button = st.form_submit_button("Submit")
