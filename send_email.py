897e7f7c06279af9dc1a68af1eb552533308ea03
MiloszRoman <mioszroman.kontakt@gmail.com>
2025-04-16 18:34:10 +0200

    Create all program to sending emails

--- send_email.py
+++ send_email.py
@@ -1,2 +1,17 @@
 import smtplib, ssl
 import os
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "mioszroman@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "mioszroman@gmail.com"
+
+    constext = ssl.create_default_context()
+    my_message = message
+
+    with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, my_message)
